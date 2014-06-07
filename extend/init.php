2bf9859776f90fe2e7922607686a165fb17ea473
maladev <irzhy.ran@gmail.com>
2014-06-07 12:31:52 +0300

    submodule wp-smartex

--- extend/init.php
+++ extend/init.php
@@ -4,4 +4,4 @@ include_once dirname(__FILE__) . '/inc/constants.php';
 include_once dirname(__FILE__) . '/inc/autoload.php';
 include_once dirname(__FILE__) . '/inc/functions.php';
 
-//Extend::add_plugin('wp-smartex/wp-smartex');
+Extend::add_plugin('wp-smartex/wp-smartex');
